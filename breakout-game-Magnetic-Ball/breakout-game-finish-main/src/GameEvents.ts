acadcd579843ef35426e765836dc73feb659edec
Georgi31 <108354976+Georgi31@users.noreply.github.com>
2022-11-08 21:05:56 +0200

    Add files via upload

---
+++ breakout-game-Magnetic-Ball/breakout-game-finish-main/src/GameEvents.ts
@@ -0,0 +1,13 @@
+export class GameEvents {
+
+    // Game events
+    public static GAME_START: string = "GAME_START";
+    public static GAME_LOST: string = "GAME_LOST";
+    public static BRICK_HIT : string = "BRICK_HIT";
+    public static BRICK_HIDE : string = "BRICK_HIDE";
+    public static MAGNETIC_BALL : string = "MAGNETIC_BALL";
+    public static BALL_LOST : string = "BALL_LOST";
+    public static NEXT_LEVEL : string = "NEXT_LEVEL";
+    public static BALL_ACTIVE : string = "BALL_ACTIVE";
+}
+
